020d1931d5944db630dbca768062acf01e2e9a24
Atarvano <vanheldyirvan@gmail.com>
2025-04-21 16:50:59 +0700

    Fix database connection in conn.php and update database name to 'sistemkas'

--- src/php/conn.php
+++ src/php/conn.php
@@ -2,13 +2,7 @@
 $host = 'localhost';
 $user = 'root';
 $password = '';
-$database = 'your_database_name';
+$database = 'sistemkas';
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = mysqli_connect($host, $user, $password, $database);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-?>
